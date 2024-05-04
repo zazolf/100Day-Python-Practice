a2a14ff479dfb648d4e8b4dfed74d15e3f8788b8
zazolf <164220384+zazolf@users.noreply.github.com>
2024-05-04 14:44:36 +0000

    Done

---
+++ Day_9_Practice/main.py
@@ -0,0 +1,32 @@
+bid_dic = {}
+def bid_dictionary(name, bid):
+    bid_dic[name] = bid
+    print(bid_dic) 
+
+#HINT: You can call clear() to clear the output in the console.
+from art import logo
+print(logo)
+from replit import clear
+print("Welcome to the secret auction program!")
+repeat = True
+while repeat:
+    name = input("what is your name?")  
+    bid = float(input("what is your bid?"))
+    again = input("Are there any other bidders? Type 'Yes' or 'No'")
+    if again == "yes":
+        clear()
+    elif again == "no":
+        repeat = False
+
+    bid_dictionary(name, bid)
+    highest_bid = 0
+    winner = ""
+    for item in bid_dic:
+        if bid_dic[item] > highest_bid:
+            highest_bid = bid_dic[item]
+            winner = item
+
+
+print(f"The winner is {winner} with a bid of {highest_bid}")
+
+
