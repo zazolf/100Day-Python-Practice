291cd15efa093658c8cc3a4d7ee00ae66ba79129
zazolf <164220384+zazolf@users.noreply.github.com>
2024-05-10 03:25:26 +0000

    done

--- Blackjack_game_ Capstone_project_11/main.py
+++ Blackjack_game_ Capstone_project_11/main.py
@@ -3,26 +3,32 @@ import random
 from art import logo
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def score_calculate(my_list):
+  if sum(my_list) == 21 and len(my_list) == 2:
+    return 0
+  elif 11 in my_list and sum(my_list) > 21:
+    my_list.remove(11)
+    my_list.append(1)
+  return sum(my_list)
+
 
 def start_game():
 
   play = input("Do you want to play a game of Blackjack? Type 'y' or 'n'")
+  print(logo)
 
   if play == 'y':
-    print(logo)
     user_list = []
     for i in range(2):
-        user_choice = random.choice(cards)
-        user_list.append(user_choice)
-    user_current_score = user_list[0] + user_list[1]
+        user_list.append(random.choice(cards))
+    user_current_score = score_calculate(user_list)
 
     computer_list = []
     for j in range(2):
-        computer_choice = random.choice(cards)
-        computer_list.append(computer_choice)
-    computer_score = computer_list[0] + computer_list[1]
+        computer_list.append(random.choice(cards))
+    computer_score = score_calculate(computer_list)
 
-    print(f"your cards: {user_list}), current score: {user_current_score}, computer_first_card: {computer_list[0]}")
+    print(f"your cards: {user_list}, current score: {user_current_score}, computer_first_card: {computer_list[0]}")
 
     game = True
     while game:
@@ -30,14 +36,17 @@ def start_game():
         print(" you won with a jackpot!")
       elif computer_score == 21 and len(computer_list) == 2:
         print(" you lost! computer won with a jackpot!")
+      elif user_current_score > 21 and computer_score > 21:
+        print( "you lost! both went over!")
+        game = False
+      elif user_current_score > 21:
+        print(" you lost! you went over!")
+        game = False
       elif computer_score > 21:
         print(" you won! computer went over!")
         game = False
       elif user_current_score == 21:
         print(" you won!")
-        game = False
-      elif user_current_score > 21:
-        print("you lost! you went over!")
         game = False
       elif computer_score == 21:
         print("you lost!")
@@ -51,12 +60,11 @@ def start_game():
         if repeat == 'y':
          new_user_item = random.choice(cards)
          user_list.append(new_user_item)
-         user_current_score += new_user_item
+         user_current_score = score_calculate(user_list)
+         computer_score = score_calculate(computer_list)
 
+         print(f"your cards: {user_list}, current score: {user_current_score}, computer first card: {computer_list[0]}")
 
-         
-         computer_score += new_comp_item
-         print(f"your cards: {user_list}, current score: {user_current_score}, computer_first_card: {computer_list[0]}")
 
         elif repeat == 'n':
           print(f"your final hand: {user_list}, final score: {user_current_score}")
